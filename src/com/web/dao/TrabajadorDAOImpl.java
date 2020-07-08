d439695d79d0a57dbc88eaf7d3b672111aec7c85
israelpalma <ipalm001@gmail.com>
2020-07-08 18:57:32 -0400

    logica Trabajador2

---
+++ src/com/web/dao/TrabajadorDAOImpl.java
@@ -0,0 +1,57 @@
+package com.web.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.web.model.Mensaje;
+import com.web.model.Trabajador;
+import com.web.util.Conexion;
+
+public class TrabajadorDAOImpl implements TrabajadorDAO{
+	
+	Conexion cn=new Conexion();
+	Connection con ;
+	Statement stmt ;
+	ResultSet resul ;
+
+	Trabajador trabajador = new Trabajador();
+	Mensaje mensaje = new Mensaje();
+	
+	//constructor vacio
+	public TrabajadorDAOImpl() {
+		
+	}
+
+	@Override
+	public Mensaje agregarTrabajador(Trabajador trabajador) {
+		
+        String query = "INSERT INTO trabajador (nombre,email,rut,empresa_id) VALUES ( '"+ 
+        		trabajador.getsNombre()+  "' , '" +
+        		trabajador.getsEmail() + "' , '" +
+        		trabajador.getsRut() + "' , '" +
+        		trabajador.getiEmpresaID() + "' )";
+        
+        //comprobar que la query esta hecha correctamente
+        System.out.println("Query: " + query);
+        try {
+            con=cn.getConnection();
+            stmt=con.prepareStatement(query);
+            int resultado = stmt.executeUpdate(query);
+            if(resultado == 1) {
+            	mensaje.setsMensaje("Usuario agregado con exito");
+            }else {
+            	mensaje.setsMensaje("Error al registrar usuario");
+            	mensaje.setiEstado(0);
+            }
+            
+        } catch (SQLException e) {
+        	mensaje.setsMensaje("Error al ejecutar la consulta: "+ query);
+        	mensaje.setiEstado(0);
+		}
+        
+		return mensaje;
+	}
+	
+}
---
+++ src/com/web/impl/TrabajadoresMgr.java
@@ -0,0 +1,10 @@
+package com.web.impl;
+
+import com.web.model.Mensaje;
+import com.web.model.Trabajador;
+
+public interface TrabajadoresMgr {
+
+	Mensaje agregarTrabajador(Trabajador trabajador);
+
+}
